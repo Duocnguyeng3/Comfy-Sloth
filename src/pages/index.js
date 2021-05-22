af9da1aad92551fdd2875aa12e462184e826659c
Duocnguyeng3 <duocnguyen.g3@gmail.com>
2021-05-22 18:06:24 +0700

    check css issue

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import About from "./AboutPage.js";
+import AuthWrapper from "./AuthWrapper.js";
+import Cart from "./CartPage.js";
+import Checkout from "./CheckoutPage.js";
+import Error from "./ErrorPage.js";
+import Home from "./HomePage.js";
+import PrivateRoute from "./PrivateRoute.js";
+import Products from "./ProductsPage.js";
+import SingleProduct from "./SingleProductPage.js";
+
+export {
+  About,
+  AuthWrapper,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+};
---
+++ src/utils/helpers.js
@@ -0,0 +1,8 @@
+export const formatPrice = (price) => {
+  return Intl.NumberFormat("en-Us", {
+    style: "currency",
+    currency: "USD",
+  }).format(price / 100);
+};
+
+export const getUniqueValues = () => {};
